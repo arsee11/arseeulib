271cc42103fa234ac85c2140190af6605fdc3b3d
arsee11 <arsee@sina.com>
2014-11-27 16:44:38 +0800

    c in gcc

--- mvc/server.h
+++ mvc/server.h
@@ -79,7 +79,7 @@ public:
 	PreactorServer(size_t max_session, SockConfig &conf)
 		:_preactor(max_session)
 	{
-		_preactor.RegistryHandler(new Acceptor(conf.lip.c_str(), conf.lport));
+		_preactor.RegistryAcceptor(new Acceptor(conf.lip.c_str(), conf.lport));
 	}
 
 	bool Run()throw()
