5d278f59e4385ffdfa17631c012c1095ff4bfb7a
arsee11 <arsee@sina.com>
2015-05-27 16:05:24 +0800

    add

--- mvc/java/pack.java
+++ mvc/java/pack.java
@@ -5,6 +5,14 @@ public class Pack{
 		this.src = src;
 		this.trgt = trgt;
 		this.act = act;
-		this.act = act;
 	}
+
+	public String getSource(){ return src;  }
+	public String getTarget(){ return trgt; }
+	public String getAction(){ return act;  }
+
+
+	String src;
+	String trgt;
+	String act;
 }
