066ba437b161b959baef216d1acee1d267d57ced
arsee11 <arsee@sina.com>
2015-06-12 10:49:28 +0800

    modify java

--- mvc/java/pack.java
+++ mvc/java/pack.java
@@ -4,10 +4,11 @@ package mylib.mvc.java;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.lang.Exception;
 
 public abstract class Pack{
 
-	static final byte[] HEAD = {(byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF};
+	public static final byte[] HEAD = {(byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF};
 	
 	public static class ParamTable extends ArrayList<HashMap<String, Object>>{
 		public void AddColumn(String name, Object value){
@@ -17,31 +18,7 @@ public abstract class Pack{
 		}
 	}
 	
-	public static int bytes2Int(byte[] bytes, int offset)
-	{
-		if( bytes.length-offset < 4 )
-			return 0;
-			
-		int value=0;
-		value = value & ((int)buf[0+offset]);
-		value = value & ((int)buf[1+offset])<<8;
-		value = value & ((int)buf[2+offset])<<16;
-		value = value & ((int)buf[3+offset])<<24;
-	
-		return value;
-	}
-	
-	public static void int2Bytes(int value, byte[] bytes, int offset)
-	{
-		if( bytes != null && bytes.length-offset>4)
-		{
-			bytes[offset+0] = (byte)(value&0xFF); 	   	 //1st byte;
-			bytes[offset+1] = (byte)((value>>8 )&0xFF); //2nd byte;
-			bytes[offset+2] = (byte)((value>>16)&0xFF); //3nd byte;
-			bytes[offset+3] = (byte)((value>>24)&0xFF); //4nd byte;
-		}
-	}
-	
+
 	public abstract class Serializer{
 		public byte[] solve(){
 			StringBuffer strbuf = new StringBuffer();			
@@ -49,8 +26,12 @@ public abstract class Pack{
 			int len = str.length();
 			byte[] buf = new byte[8+len];
 			System.arraycopy(HEAD, 0, buf, 0, 4);
-			int2Bytes(len, buf, 4);
-			
+			try{
+				Util.int2Bytes(len, buf, 4);	
+			}catch(Exception e){
+				return null;
+			}
+
 			System.arraycopy(str.getBytes(), 0, buf, 8, len);
 			return buf;
 		}
@@ -59,38 +40,64 @@ public abstract class Pack{
 	}
 	
 	public abstract class UnSerializer{
-		public boolean solve(byte[] buf){
-			for(int i=0; i<4; i++){
-				if(buf[i] != (byte)0xFF)
-					return false;
+		public void solve(byte[] buf) throws Exception{
+			if( buf.length < 8 )
+				throw new Exception("buf to small");
+
+			int payloadLen=0; 
+			try{
+				if( Util.bytes2Int(buf, 0) != Util.bytes2Int(HEAD, 0) )
+					throw new Exception("head failed");
+
+				payloadLen = Util.bytes2Int(buf, 4);
+				System.out.println("payloadLen:"+payloadLen);
+				if( buf.length-8 < payloadLen )
+				{
+					setStatus(false);
+					return;
+				}
+			}catch(Exception e){
+				throw e;
 			}
-					
-			int len = bytes2Int(buf, 4);
-			return parseBody(buf, 8, len);
+
+			if( !parseBody(buf, 8, payloadLen) )
+				throw new Exception("payload invalid");
+
 		}
-		
-		abstract boolean parseBody(byte[] buf, int offset, int len);
+
+		public abstract boolean parseBody(byte[] buf, int offset, int len);
 	}
-	
+
+	public Pack(){ this.status = false; }
 	public Pack( String src, String trgt, String act ){
 		this.src = src;
 		this.trgt = trgt;
 		this.act = act;
 	}
 	
-	public abstract Serializer getSerializer();
-	public abstract UnSerializer getUnSerializer();
-	
 	public String getSource(){ return src;  }
 	public String getTarget(){ return trgt; }
 	public String getAction(){ return act;  }
+	public Object getParam(int i, String name)
+	{
+		if( paramTable.size() > i )
+			return paramTable[i].get(name);
+			
+		return new String();
+	}
+	
+	public boolean getStatus(){ return status;  }
+	public void setStatus(boolean value ){ status=value;  }
 	
 	public void  setParamTable(ParamTable table){ paramTable = table;  }
 	public Pack.ParamTable getParamTable(){ return paramTable;    }
 
+	public abstract Serializer getSerializer();
+	public abstract UnSerializer getUnSerializer();
 	
-	String src;
-	String trgt;
-	String act;
-	ParamTable paramTable =  new ParamTable();
+	protected String src;
+	protected String trgt;
+	protected String act;
+	protected boolean status;
+	protected ParamTable paramTable =  new ParamTable();
 }
