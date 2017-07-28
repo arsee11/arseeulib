0cac7adeee7b1084dfc5339c4c74c42e920cb49a
Huang Liguan <arsee@sina.com>
2017-07-28 13:32:14 +0800

    Update executable_queue.h

--- threading/executable_queue.h
+++ threading/executable_queue.h
@@ -1,5 +1,8 @@
 //@file executable_queue.h
 
+#ifndef EXECUTABLE_QUEUE_H
+#define EXECUTABLE_QUEUE_H
+
 #include <queue>
 #include <functional>
 #include <mutex>
@@ -9,38 +12,40 @@ typedef std::function<void()> exec_object_t;
 class ExecutableQueue
 {
 public:
-	ExecutableQueue()
-	{
-	}
+    ExecutableQueue()
+    {
+    }
 
-	void post(exec_object_t& f)
-	{
-		std::lock_guard<std::mutex> g(_mutex);
-		_work_queue.push(f);
-	}
+    void post(exec_object_t& f)
+    {
+        std::lock_guard<std::mutex> g(_mutex);
+        _exec_queue.push(f);
+    }
 
-	void post(exec_object_t&& f)
-	{
-		std::lock_guard<std::mutex> g(_mutex);
-		_work_queue.push(f);
-	}
-  
- 	void exec()
-	{
-		exec_object_t func = nullptr;
-		_mutex.lock();
-		if(_work_queue.size() > 0)
-		{
-			func = _work_queue.front();
-		  	_work_queue.pop();
-		}
-		_mutex.unlock();
+    void post(exec_object_t&& f)
+    {
+        std::lock_guard<std::mutex> g(_mutex);
+        _exec_queue.push(f);
+    }
 
-		if(func != nullptr)
-			func();
-	}
-  
+    void exec()
+    {
+        exec_object_t func = nullptr;
+        _mutex.lock();
+        if(_exec_queue.size() > 0)
+        {
+            func = _exec_queue.front();
+            _exec_queue.pop();
+        }
+        _mutex.unlock();
+
+        if(func != nullptr)
+            func();
+    }
+
 private:
-	std::queue<exec_object_t> _exec_queue;
-	std::mutex _mutex;
+    std::queue<exec_object_t> _exec_queue;
+    std::mutex _mutex;
 };
+
+#endif // EXECUTABLE_QUEUE_H
