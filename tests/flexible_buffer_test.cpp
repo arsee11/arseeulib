747cb2a31dc005507c85c402a55c13dbedf6dbb7
arsee <arsee@sina.com>
2018-04-11 23:56:48 +0800

    add flexible_buffer.h

---
+++ tests/flexible_buffer_test.cpp
@@ -0,0 +1,64 @@
+
+#include "../flexible_buffer.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	FlexibleBuffer<char> fb(100);
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+
+	cout<<"*********************************************"<<endl;
+	const char* data="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX";
+	fb.push(data, 50);
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+
+	cout<<"*********************************************"<<endl;
+	char* tdata=nullptr;
+	size_t size=0;
+	std::tie(tdata, size)=fb.head(50);
+	cout<<"head(50):"<<size<<", data:"<<tdata<<endl;
+	std::tie(tdata, size)=fb.head(60);
+	cout<<"head(60):"<<size<<", data:"<<tdata<<endl;
+
+	cout<<"*********************************************"<<endl;
+	cout<<"cunsume(40):"<<fb.consume(40)<<endl;
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+	std::tie(tdata, size)=fb.head(60);
+	cout<<"head(60):"<<size<<", data:"<<tdata<<endl;
+
+	cout<<"*********************************************"<<endl;
+	cout<<"cunsume(40):"<<fb.consume(40)<<endl;
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+	std::tie(tdata, size)=fb.head(60);
+	cout<<"head(60):"<<size<<", data:"<<tdata<<endl;
+	
+	cout<<"*********************************************"<<endl;
+	fb.clear();
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+	std::tie(tdata, size)=fb.head(60);
+	cout<<"head(60):"<<size<<", data:"<<tdata<<endl;
+	
+	cout<<"*********************************************"<<endl;
+	fb.push(data, 50);
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+	fb.push(data, 50);
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+	fb.push(data, 50);
+	cout<<"capacity="<<fb.capacity()<<endl;
+	cout<<"size="<<fb.size()<<endl;
+	std::tie(tdata, size)=fb.head(fb.size());
+	cout<<"head:"<<size<<", data:"<<tdata<<endl;
+	
+	return 0;
+
+}
